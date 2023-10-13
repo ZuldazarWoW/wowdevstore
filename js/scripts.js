8c3be269c3373772415ced59dd38460fecd4cfc3
Sebastian Godoy <sego.du@hotmail.com>
2023-10-13 11:07:37 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,112 +1,88 @@
-// Obtén el parámetro "category" y "product" de la URL
-const urlParams = new URLSearchParams(window.location.search);
-const category = urlParams.get("category");
-const productIndex = urlParams.get("product");
+$(document).ready(function () {
+    // Obtén el parámetro "category" de la URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const category = urlParams.get("category");
 
-// Lista de categorías y sus nombres
-const categories = {
-    themes: "Themes",
-    modules: "Modules"
-    // Agrega más categorías según sea necesario
-};
+    // Lista de categorías y sus nombres
+    const categories = {
+        themes: "Themes",
+        modules: "Modules"
+        // Agrega más categorías según sea necesario
+    };
 
-// Función para cargar productos basados en la categoría
-function loadProducts(category) {
-    const categoryTitle = categories[category] || "Categoría Desconocida";
-    $("#category-title").text(categoryTitle);
-    const productCardsContainer = $("#product-cards");
+    // Función para cargar productos basados en la categoría
+    function loadProducts(category) {
+        const categoryTitle = categories[category] || "Categoría Desconocida";
+        $("#category-title").text(categoryTitle);
+        const productCardsContainer = $("#product-cards");
 
-    if (productIndex === null) {
-        // Cargar todos los productos de la categoría
-        for (let i = 0; i < 2; i++) { // Carga productos 0 y 1
-            $.getJSON(`categories/${category}/product${i}.json`, function (product) {
-                const card = `
-                    <div class="col-md-4 mb-3">
-                        <div class="card">
-                            <img src="${product.image}" class="card-img-top" alt="${product.title}">
-                            <div class="card-body">
-                                <h5 class="card-title">${product.title}</h5>
-                                <p class="card-text">${product.description}</p>
-                                <p class="card-text">Precio: ${product.price}</p>
-                                <a class="btn btn-primary" href="categories.html?category=${category}&product=${i}">Ver más</a>
-                            </div>
-                        </div>
-                    </div>
-                `;
-                productCardsContainer.append(card);
-            });
-        }
-    } else {
-        // Cargar un producto específico
-        $.getJSON(`categories/${category}/product${productIndex}.json`, function (product) {
-            const card = `
-                <div class="col-md-4 mb-3">
-                    <div class="card">
-                        <img src="${product.image}" class="card-img-top" alt="${product.title}">
-                        <div class="card-body">
-                            <h5 class="card-title">${product.title}</h5>
-                            <p class="card-text">${product.description}</p>
-                            <p class="card-text">Precio: ${product.price}</p>
-                        </div>
-                    </div>
-                </div>
-            `;
-            productCardsContainer.append(card);
-        }).fail(function () {
-            // Si hay un error al cargar el producto, muestra un mensaje de error.
-            $("#category-title").text("Error al cargar el producto");
+        // Cargar la lista de productos de la categoría
+        $.ajax({
+            url: `categories/${category}`,
+            success: function (data) {
+                // Recorre los archivos .md en la carpeta de la categoría
+                $(data).find("a:contains(.md)").each(function () {
+                    const productFile = $(this).attr("href");
+                    $.ajax({
+                        url: `categories/${category}/${productFile}`,
+                        success: function (productData) {
+                            const product = parseProductData(productData);
+                            const card = `
+                                <div class="col-md-4 mb-3">
+                                    <div class="card">
+                                        <img src="${product.image}" class="card-img-top" alt="${product.title}">
+                                        <div class="card-body">
+                                            <h5 class="card-title">${product.title}</h5>
+                                            <p class="card-text">Precio: ${product.price}</p>
+                                            <a class="btn btn-primary" href="product.html?category=${category}&product=${productFile}">Ver más</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            `;
+                            productCardsContainer.append(card);
+                        }
+                    });
+                });
+            }
         });
     }
-}
 
-// Función para mostrar detalles de un producto
-function showProductDetails(index, category) {
-    // Cargar el producto desde un archivo separado
-    $.getJSON(`categories/${category}/product${index}.json`, function (product) {
-        const modal = `
-            <div class="modal fade" tabindex="-1" role="dialog">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title">${product.title}</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <img src="${product.image}" class="img-fluid" alt="${product.title}">
-                            <p>${product.description}</p>
-                            <p>Precio: ${product.price}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        `;
-        $(modal).modal("show");
-    }).fail(function () {
-        // Si hay un error al cargar el producto, muestra un mensaje de error.
-        alert("Error al cargar el producto");
-    });
-}
+    // Función para mostrar detalles de un producto
+    function showProductDetails(category, productFile) {
+        $.ajax({
+            url: `categories/${category}/${productFile}`,
+            success: function (productData) {
+                const product = parseProductData(productData);
+                // Aquí puedes mostrar los detalles del producto y el botón de compra
+                // Redirigir a WhatsApp con el mensaje
+                const whatsappMessage = encodeURIComponent(`Estoy interesado en comprar ${product.title}`);
+                window.location.href = `https://wa.me/TUNUMERODEWHATSAPP/?text=${whatsappMessage}`;
+            }
+        });
+    }
 
-// Cargar enlaces a las categorías en la página de inicio
-$(document).ready(function () {
+    // Función para analizar los datos del producto desde el archivo .md
+    function parseProductData(data) {
+        const metadata = data.match(/---([\s\S]*?)---/)[1];
+        const content = data.split(/---([\s\S]*?)---/)[2].trim();
+        const product = {};
+        metadata.split("\n").forEach(function (item) {
+            const parts = item.split(":");
+            const key = parts[0].trim();
+            const value = parts[1].trim();
+            product[key] = value;
+        });
+        product.description = content;
+        return product;
+    }
+
+    // Cargar enlaces a las categorías en la página de inicio
     const categoryLinksContainer = $("#category-links");
-
     for (const cat in categories) {
         categoryLinksContainer.append(`<a class="btn btn-primary mr-2" href="categories.html?category=${cat}">${categories[cat]}</a>`);
     }
 
     if (category) {
-        if (productIndex) {
-            showProductDetails(productIndex, category);
-        } else {
-            loadProducts(category);
-        }
-    } else {
-        // Si no hay categoría en la URL, carga todos los productos en la página de inicio
-        for (const cat in categories) {
-            loadProducts(cat);
-        }
+        loadProducts(category);
     }
 });
